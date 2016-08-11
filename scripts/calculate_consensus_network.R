0ca3016f039f04c5eecb80cbfa473183de51f397
Raphael <raphaelpeer@hotmail.com>
2016-08-11 16:03:35 +0200

    "docstring" added; comments and error-checking improved in calculate_consensus.R

--- scripts/calculate_consensus_network.R
+++ scripts/calculate_consensus_network.R
@@ -1,11 +1,46 @@
-# cat(sprintf('argument1: %s\n', args[[1]]))
-# cat(sprintf('argument2: %s\n', args[[2]]))
-#setwd('/home/raph/resint/ras/pipeline')  # THIS LINE MUST GO, find out why working directory is not set to directory of script
-# nw <-  read.csv('results/raw_networks.csv', stringsAsFactors=F)
-# mapping <- read.csv('results/mapping.csv', stringsAsFactors=F)
+# ------------------------------------------------------------------------------------------
+# AUTHOR: Raphael Peer, raphael1peer@gmail.com
+#
+# PURPOSE:
+# Individual residue contact networks are combined into a consensus residue contact network
+# via a a common residue numbering system defined by a reference alignment. In short, for
+# every contact present in any structure, the script determines how many other structures
+# have an equivalent contact.
+#
+# INPUT:
+# 1) 'raw' residue contact networks csv-file: For every contact, the file provided PDB-ID,
+# PDB-number of the first residue and PDB-number of the second residue.
+# 2) mapping csv-file: For every residue, the file provides PDB-number as well as the
+# position in the reference alignment. Furthermore, the equivalent residue in the reference
+# structure (i.e. same column in the reference alignment) is given.
+# The two input files are written by the python scripts 'calculate_networks.py' and 
+# 'map_networks.py', respectively. Further information is provided in the docstring and
+# comments of those scripts.
+#
+# OUTPUT:
+# 1) mapped networks csv-file: Contains the same information as the first input file with
+# additional information for each contact: alignment positions, equivalent residues in the
+# reference structure, amino acid, etc. All this information is provided for both residues
+# forming the contact (termed A and B).
+# 2) MAIN RESULT: consensus network scv-file: The file provides two alignment positions
+# if at least one structure has a contact between the two residues. The number of
+# structures which have an equivalent residue contact is given in the column 'contact_num'.
+# The column 'conservation' gives the fraction of the number of structure which have a
+# contact over the total number of structures in the dataset. The value is very small the
+# contact is present in only a single structure. A contact present in all structures of
+# the dataset has the value 1. The last two columns give the PDB-number of the equivalent
+# residues in the reference PDB-structure. (For instance, this information can be used
+# to visualize the higly conserved residue contacts ('conservation' 1 or close to 1) on
+# the reference structure.)
+# ------------------------------------------------------------------------------------------
+
+# nw <-  read.csv('../results/raw_networks.csv', stringsAsFactors=F)
+# mapping <- read.csv('../results/mapping.csv', stringsAsFactors=F)
 args <- commandArgs(trailingOnly = TRUE)
 nw <- read.csv(args[[1]], stringsAsFactors=F)
 mapping <- read.csv(args[[2]], stringsAsFactors=F)
+require(Biostrings)
+alignment <- readAAStringSet('data/ras_reference_alignment.fa', 'fasta')
 
 
 require(plyr)
@@ -21,50 +56,48 @@ nw <- ddply(nw, 'pdb_id', function(x) {  # map common numbering to network
 
 unmapped_networks <- unique(subset(nw, is.na(alignment_pos_A) | is.na(alignment_pos_B))$pdb_id)
 if (length(unmapped_networks) > 0 ) {
-  cat('WARNING: There are some contacts not mapped to an alignment position in these networks:\n',
+  cat('Note: Some contacts are not mapped to an alignment position in these networks:\n',
     unique(subset(nw, is.na(alignment_pos_A) | is.na(alignment_pos_B))$pdb_id), '\n',
     'Unmapped contacts will simply be excluded form consensus network calculation.\n',
     'They should not cause a problem if the remaining dataset is large enough.')
 }
 
-nw$seq_prox <- nw$res_B - nw$res_A  # proximity in sequence between the two contacting residues
-#nw <- subset(nw, seq_prox > 4)  # exclude short range interactions (excluces about half of the contacts)
-nw <- nw[,c('pdb_id', 'res_A', 'pdb_A', 'alignment_pos_A', 'aa_A', 'ref_pdb_A', 'res_B', 'pdb_B', 'alignment_pos_B', 'aa_B', 'ref_pdb_B', 'seq_prox')]
-#nw <- nw[,c('pdb_id', 'res_A', 'pdb_A', 'alignment_pos_A', 'res_B', 'pdb_B', 'alignment_pos_B', 'seq_prox')]
+nw$seq_prox <- nw$res_B - nw$res_A  # proximity in sequence between the two residues
+#nw <- subset(nw, seq_prox > 4)  # exclude short range interactions (half of the contacts)
+nw <- nw[,c('pdb_id', 'res_A', 'pdb_A', 'alignment_pos_A', 'aa_A', 'ref_pdb_A',
+            'res_B', 'pdb_B', 'alignment_pos_B', 'aa_B', 'ref_pdb_B', 'seq_prox')]
 write.csv(nw, 'results/mapped_networks.csv', row.names=F, quote=F)
-
-
-require(Biostrings)
-alignment <- readAAStringSet('data/ras_reference_alignment.fa', 'fasta')
 
 
 nw_compact <- nw[,c('pdb_id', 'alignment_pos_A', 'alignment_pos_B')]
 nw_compact <- na.omit(nw_compact)
 
-# if (nrow(nw_compact) != nrow(na.omit(nw_compact))) {
-#   cat('\nNOTE: unmapped contacts have been excluded from the analysis.\n')
-#   nw_compact <- na.omit(nw_compact)
-# }
-
 # CHECK IF ALL STRUCTURES ARE IN ALIGNMENT:
-check <- paste0(unique(nw_compact$pdb_id), '.pdb') %in% names(alignment)
+structures <- paste0(unique(nw_compact$pdb_id), '.pdb')
+not_in_alignment <- structures[! structures %in% names(alignment)]
+if (length(not_in_alignment) > 0) {
+  cat('\nNOTE: The following structures have no corresponding sequences in the reference
+      alignment. Therefore, they are excluded from further analysis. Unless you require
+      those structures in the dataset, this is not a problem. If you want those structures
+      inculded in the further analysis, please add their sequences to the reference
+      alignment and restart the analysis.')
+}
+
 
 consensus <- ddply(nw_compact, c('alignment_pos_A', 'alignment_pos_B'), function(x) {
   cons <- unique(x[,c('alignment_pos_A', 'alignment_pos_B')])
-  cons$contact_num <- nrow(x)
-  alignment_columns <- as.matrix(alignment)[,c(cons$alignment_pos_A, cons$alignment_pos_B)]  # columns of the two contacting residues
-  colnames(alignment_columns) <- c('res_a', 'res_b')
-  alignment_columns <- as.data.frame(alignment_columns)
-  #cons$possible_contacts <- nrow(subset(alignment_columns, res_a != '-' & res_b != '-'))
-  cons$conservation <- cons$contact_num / length(unique(nw_compact$pdb_id)) # ATTENTION: new, stricter way of calculating contact conservation
+  cons$contact_num <- nrow(x)  # number of structure which have an equivalent contact
+  # Fraction of structures which have an equivalent contact:
+  cons$conservation <- cons$contact_num / length(unique(nw_compact$pdb_id))
   return(cons)
 })
-#consensus$contact_num <- consensus$possible_contacts <- NULL
 
 # map consensus network to PDB-positions of reference structure
 alignment_to_reference <- unique(mapping[,c('alignment_pos', 'ref_pdb')])
-consensus <- merge(consensus, alignment_to_reference, by.x='alignment_pos_A', by.y='alignment_pos')
-consensus <- merge(consensus, alignment_to_reference, by.x='alignment_pos_B', by.y='alignment_pos', suffixes=c('_A', '_B'))
+consensus <- merge(consensus, alignment_to_reference, by.x='alignment_pos_A',
+                   by.y='alignment_pos')
+consensus <- merge(consensus, alignment_to_reference, by.x='alignment_pos_B',
+                   by.y='alignment_pos', suffixes=c('_A', '_B'))
 
 
 write.csv(consensus, 'results/consensus_network.csv', row.names=F, quote=F)
